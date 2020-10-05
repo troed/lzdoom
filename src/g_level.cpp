eeea8854faafb7f61f1691795cfe7ae31d9323f1
drfrag <drfrag666@hotmail.com>
2020-10-05 18:02:13 +0200

    - Fixed: NetServerInfo was not being saved after the exit cleanup.

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -380,7 +380,6 @@ void G_NewInit ()
 	primaryLevel->FraggleScriptThinker = nullptr;
 
 	G_ClearSnapshots ();
-	netgame = false;
 	multiplayer = multiplayernext;
 	multiplayernext = false;
 	if (demoplayback)
