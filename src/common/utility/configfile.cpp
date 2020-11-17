a886e24574563d0f3655752fbbbe17ba068b396f
drfrag <drfrag666@hotmail.com>
2020-11-17 20:50:50 +0100

    Revert "- Fixed config file being overriden when it's blocked by another application."
    This reverts commit 841696408c6460b8d02de17f0ccd9074ef144b35.

--- src/common/utility/configfile.cpp
+++ src/common/utility/configfile.cpp
@@ -39,12 +39,6 @@
 #include "configfile.h"
 #include "files.h"
 
-#ifdef _WIN32
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "engineerrors.h"
-#endif
-
 #define READBUFFERSIZE	256
 
 //====================================================================
@@ -606,24 +600,13 @@ void FConfigFile::LoadConfigFile ()
 {
 	FileReader file;
 	bool succ;
-#ifdef _WIN32
-	struct _stat buf;
 
-	if (_stat(PathName.GetChars(), &buf) != 0)
-		FileExisted = false;
-
+	FileExisted = false;
 	if (!file.OpenFile (PathName))
 	{
-		if (!FileExisted) return;
-		else I_Error ("Could not Open Config file.\n");
-	}
-#else
-	if (!file.OpenFile (PathName))
-	{
-		FileExisted = false;
 		return;
 	}
-#endif
+
 	succ = ReadConfig (&file);
 	FileExisted = succ;
 }
